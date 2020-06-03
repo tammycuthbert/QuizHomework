0ccb05414cbeca5a07ee86475c33852831b976e2
Tammy Cuthbert <tammycuthert427@gmail.com>
2020-06-02 23:30:59 -0500

    modified/assets/script file

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,12 @@ var htmlButton=document.getElementsByClassName("this");
 var sectionIntro=document.querySelector("#intro")
 var startButton=document.getElementById("begin")
 var Line20=document.querySelector("#questions")
-var startQuestion=document.querySelector("#questions")
+var startQuestions=document.querySelector("#questions")
 
 startButton.addEventListener("click",function(){
     sectionIntro.classList.add("hide")
 })
 
-startQuestion.addEventListener()
+startQuestions.addEventListener("questions",function(){
+    sectionQuestions.ClaasList.add("")
+})
