d6e27c2e65e7a1fae34494833a460b384d21ce9b
Tammy Cuthbert <tammycuthert427@gmail.com>
2020-06-02 22:31:17 -0500

    changes to html and javascript

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var htmlButton=document.getElementsByClassName("this");
+var sectionIntro=document.querySelector("#intro")
+var startButton=document.getElementById("begin")
+var Line20=document.querySelector("#questions")
+var startQuestion=document.querySelector("#questions")
+
+startButton.addEventListener("click",function(){
+    sectionIntro.classList.add("hide")
+})
+
+startQuestion.addEventListener()
